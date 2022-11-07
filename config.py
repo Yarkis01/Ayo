985fae5e3d485a7c25cd0e3a77dc4ea7ae7e0f31
Yarkis <109750019+Yarkis01@users.noreply.github.com>
2022-11-07 21:27:21 +0100

    Création d'un fichier de configuration vide

---
+++ config.py
@@ -0,0 +1,34 @@
+TOKEN    = ""                                           # Token Discord (str)
+GUILD_ID =                                              # ID du serveur de développement / support (int)
+VERSION  = "1.0.1"                                      # Version du bot (str)
+
+TIMEZONE = "Europe/Paris"                               # Timezone (str) 
+
+HEADERS = {                                             # Utilisé pour effectuer les requêtes vers les API publiques (dict)
+    "User-Agent": "Ayo Discord Bot (Yarkis#0397)",
+    "From": "yarkis@ik.me"
+}
+
+SPLATOON3_API = "https://splatoon3.ink/data"            # Lien de l'API Splatoon3.ink (str)
+SPLATOON2_API = "https://splatoon2.ink/data"            # Lien de l'API Splatoon2.ink (str)
+
+ROTATION_CHANNEL_ID =                                   # ID du salon textuel qui recevra les messages automatiques de rotations (int)
+ROTATION_ROLES_ID   =                                   # Rôle à mentionner quand le bot poste les messages de rotation (int)
+
+SUPPORT_SERVER = ""                                     # Lien d'invitation vers le serveur de support (str)
+ADD_BOT_LINKS  = ""                                     # Lien d'invitation permettant d'inviter le bot (str)
+
+ERROR_CHANNEL_ID = 
+
+JOIN2CREATE = [
+    ,                                                   # ID du salon permettant la création d'un salon vocal avec 2 emplacements (int)
+    ,                                                   # ID du salon permettant la création d'un salon vocal avec 4 emplacements (int)
+    ,                                                   # ID du salon permettant la création d'un salon vocal avec 10 emplacements (int)
+                                                        # Lien de la catégorie qui contiendra les salons vocaux créés et ceux permettant d'en créer (int)
+]
+
+BOT_API_URL = ""                                        # Lien vers l'API Pterodactyl (str)
+BOT_API_KEY = ""                                        # Clé d'authentification de l'API Pterodactyl (str)
+
+WATCHBOT_API_URL = ""                                   # Lien vers l'API Watchbot.app (str)
+WATCHBOT_API_KEY = ""                                   # Clé d'authentification de l'API Watchbot.app (str)
