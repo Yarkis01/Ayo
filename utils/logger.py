18ccfd9d3bb22a051ab8948d17910d5f545bd76a
Vincent <109750019+Yarkis01@users.noreply.github.com>
2023-06-18 13:41:00 +0200

    fix: Implémentation du "log_custom"

--- utils/logger.py
+++ utils/logger.py
@@ -9,59 +9,63 @@ class bcolors:
 
 class logs:
     @staticmethod
-    def print_log(message: str, log_type: str) -> None:
+    def print_log(message: str, log_type: str, log_custom: str = "") -> None:
         """
         Print a log message with the specified log type and message.
 
         Args:
             message (str): The log message.
             log_type (str): The log type (e.g., INFO, SUCCESS, WARNING, FAIL).
+            log_custom (str): Optional type of the log message.
         """
         timestamp = datetime.datetime.now().strftime('%H:%M:%S')
         log_color = getattr(bcolors, log_type.upper(), "")
 
-        print(f"{log_color}[{timestamp}][{log_type.upper()}]: {message}{bcolors.ENDC}")
+        if log_custom:
+            print(f"{log_color}[{timestamp}][{log_type.upper()}][{log_custom.upper()}]: {message}{bcolors.ENDC}")
+        else:
+            print(f"{log_color}[{timestamp}][{log_type.upper()}]: {message}{bcolors.ENDC}")
 
     @staticmethod
-    def warning(message: str, log_type: str = "") -> None:
+    def warning(message: str, log_custom: str = "") -> None:
         """
         Print a warning log message.
 
         Args:
             message (str): The log message.
-            log_type (str): Optional type of the log message.
+            log_custom (str): Optional type of the log message.
         """
-        logs.print_log(message, "WARNING")
+        logs.print_log(message, "WARNING", log_custom)
 
     @staticmethod
-    def success(message: str, log_type: str = "") -> None:
+    def success(message: str, log_custom: str = "") -> None:
         """
         Print a success log message.
 
         Args:
             message (str): The log message.
-            log_type (str): Optional type of the log message.
+            log_custom (str): Optional type of the log message.
         """
-        logs.print_log(message, "SUCCESS")
+        logs.print_log(message, "SUCCESS", log_custom)
 
     @staticmethod
-    def fail(message: str, log_type: str = "") -> None:
+    def fail(message: str, log_custom: str = "") -> None:
         """
         Print a fail log message.
 
         Args:
             message (str): The log message.
-            log_type (str): Optional type of the log message.
+            log_custom (str): Optional type of the log message.
         """
-        logs.print_log(message, "FAIL")
+        logs.print_log(message, "FAIL", log_custom)
 
     @staticmethod
-    def info(message: str, log_type: str = "") -> None:
+    def info(message: str, log_custom: str = "") -> None:
         """
         Print an info log message.
 
         Args:
             message (str): The log message.
-            log_type (str): Optional type of the log message.
+            log_custom (str): Optional type of the log message.
         """
-        logs.print_log(message, "INFO")
+        logs.print_log(message, "INFO", log_custom)
